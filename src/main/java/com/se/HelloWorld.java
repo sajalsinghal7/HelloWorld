7baa08061790731bf8f4e3d17d6b36d164e5d716
sajal singhal <sajalsinghal7@gmail.com>
2019-02-04 15:39:11 +0530

    initial commit

---
+++ src/main/java/com/se/HelloWorld.java
@@ -0,0 +1,6 @@
+package com.se;
+
+public class HelloWorld {
+    public void printer() {
+    }
+}
---
+++ src/test/java/com/se/HelloWorldTest.java
@@ -0,0 +1,28 @@
+package com.se;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void printName1() {
+
+    }
+
+    @Test
+    public void printName2() {
+
+    }
+}
